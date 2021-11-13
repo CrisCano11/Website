ebc1967da463ad39c179ed25654faa469093d033
CrisCano <criscano_1994@hotmail.com>
2021-11-13 09:19:47 -0500

    Correccion de peso imagenes y funcionalidad  menu

--- script.js
+++ script.js
@@ -4,4 +4,34 @@
     $(this).toggleClass('fa-times');
     $('header').toggleClass('toggle');
   })
- })
+
+ $('#educat').click(function () {
+  $('#education').focus();
+  $('#menu').toggleClass('fa-times');
+  $('header').removeClass('toggle');
+ });
+
+ $('#hom').click(function () {
+  $('#home').focus();
+  $('#menu').toggleClass('fa-times');
+  $('header').removeClass('toggle');
+ });
+
+ $('#port').click(function () {
+  $('#portfolio').focus();
+  $('#menu').toggleClass('fa-times');
+  $('header').removeClass('toggle');
+ });
+
+ $('#cont').click(function () {
+  $('#contact').focus();
+  $('#menu').toggleClass('fa-times');
+  $('header').removeClass('toggle');
+ });
+
+ $('#abou').click(function () {
+  $('#about').focus();
+ });
+
+ });
+
