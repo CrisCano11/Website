b856c92c4a18f1764e8d02861b29e4c9c8de1322
CrisCano <criscano_1994@hotmail.com>
2021-12-14 13:34:10 -0500

    Ubicaciones a las fotos y adicion al menu

--- js/send.php
+++ js/send.php
@@ -1,6 +1,6 @@
 <?php
 
-$dato = array('success' => 0, 'message' => 'Error inesperado!');
+/*$dato = array('success' => 0, 'message' => 'Error inesperado!');
 
 $nombre = $_POST['nombre'];
 $email = $_POST['email'];
@@ -15,9 +15,41 @@ mail($to,$subject,$message);
 
 $dato['success'] = true;
 
-return $dato;
+return $dato;*/
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require("PHPMailer.php");
+require("SMTP.php");
+
+$destinatario = "andyvivas27@gmail.com";
+$smtpUser = "soporte@idm.cristhian-cano.com";
+$smtpClave = "Thomas11*";
+$smtpHost = "smtp.hostinger.com";
+
+$mail = new PHPMailer();
+
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;  
+$mail->isSMTP();
+$mail->Host = $smtpHost;
+$mail->SMTPAuth = true;
+$mail->Username = $smtpUser;
+$mail->Password = $smtpClave;
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; 
+$mail->Port = 465;
+$mail->setFrom('soporte@idm.cristhian-cano.com', 'Prueba Correo');
+$mail->addAddress($destinatario);
+$mail->addAttachment("../downloads/cv.pdf");
+
+$mail->isHTML(true);
+$mail->Subject = "Prueba con Adjunto";
+$mail->Body = "Body del correo enviado desde PHP con adjunto";
+//$mail->CharSet = "utf-8";
 
 
+echo $mail->send();
 
 
 ?>
